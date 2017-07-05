3e75f5a0b4af51290619c021dd5025aa6e46f47b
SchoolGangsta <vlad.mail.ua@gmail.com>
2017-07-05 17:35:17 +0300

    initial commit

--- app/src/main/java/com/example/android/lesson/MainActivity.java
+++ app/src/main/java/com/example/android/lesson/MainActivity.java
@@ -15,15 +15,24 @@
  */
 package com.example.android.lesson;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
     }
+
+    public void openNumbersList(View view) {
+        Intent Numbers = new Intent(this, NumbersActivity.class);
+        startActivity(Numbers);
+
+    }
+
 }
